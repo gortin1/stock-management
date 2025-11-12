dba8719970616d917f29482201a440ee6be97480
gortin <bielsanfru@gmail.com>
2025-11-12 09:24:07 -0300

    fix: adição de outra url

--- src/main/java/com/api/stock_management/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/api/stock_management/infrastructure/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-                "https://front-stock-management-gortin1s-projects.vercel.app/"));
+                "https://front-stock-management-gortin1s-projects.vercel.app/",
+                "https://stockmanagement-project.vercel.app/"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
